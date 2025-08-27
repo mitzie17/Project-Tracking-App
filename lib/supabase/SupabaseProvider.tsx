82981764beadfa1f56fef6fdb475089d4ff3bfbf
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-08-27 16:20:38 -0700

    define SupabaseProvider

---
+++ lib/supabase/SupabaseProvider.tsx
@@ -0,0 +1,58 @@
+// SupabaseProvider provides the supabase client
+// SupabaseProvider has a context that shares the supabase client everywhenre in the app and that supabase
+// client contains the access token from clerk
+
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { useSession } from "@clerk/nextjs";
+
+type SupabaseContext = {
+  supabase: SupabaseClient | null;
+  isLoaded: boolean;
+};
+const Context = createContext<SupabaseContext>({
+  supabase: null,
+  isLoaded: false,
+});
+
+export default function SupabaseProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { session } = useSession();
+  const [supabase, setSupabase] = useState<SupabaseClient | null>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!session) return;
+    const client = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        accessToken: () => session?.getToken(),
+      }
+    );
+
+    setSupabase(client);
+    setIsLoaded(true);
+  }, [session]);
+
+  return (
+    <Context.Provider value={{ supabase, isLoaded }}>
+      {!isLoaded ? <div> Loading...</div> : children}
+      {/* children */}
+    </Context.Provider>
+  );
+}
+
+export const useSupabase = () => {
+  const context = useContext(Context);
+  if (context === undefined) {
+    throw new Error("useSupabase needs to be inside the provider");
+  }
+
+  return context;
+};
